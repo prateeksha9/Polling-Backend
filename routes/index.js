f1ec0f64a9edcb97eb07cabb4ad86ab41384c87d
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-22 12:51:42 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 console.log("router loaded");
+const homecontroller = require("../controllers/home_controller");
 
+router.get("/", homecontroller.front);
 router.use("/question", require("./question"));
 router.use("/option", require("./option"));
 module.exports = router;
