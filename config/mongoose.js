5251c16b6eebfb97486653828ff5b88e81f7657f
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-22 13:59:38 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // const env = require("./enviorment");
 
-mongoose.connect(`mongodb+srv://prateeksha:prateeksha@0209@cluster0.ab0jx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://prateeksha:prateeksha0209@cluster0.ab0jx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 //(`mongodb://localhost/polling_dev`);
 
 const db = mongoose.connection;
