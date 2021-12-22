64afe0e5022ea763b23d2c3b06fa17be48d05219
Prateeksha Ranjan <prateeksha.67@gmail.com>
2021-12-22 16:34:25 +0530

    Update home_controller.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,33 @@
+// module.exports.front = function (req, res) {
+//   return res.render("home", {
+//     title: "Home",
+//   });
+// };
+
+const Questions = require("../models/question");
+const Options = require("../models/option");
+
 module.exports.front = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+  try {
+    // finding all the questions and returning
+    let question = await Questions.findById(req.params.id).populate({
+      path: "option",
+    });
+
+    if (question) {
+      return res.status(200).json({
+        message: "Here is the questions",
+        data: question,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Question does not does not exists",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error from the server ",
+      data: err,
+    });
+  }
 };
